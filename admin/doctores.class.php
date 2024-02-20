e85f4d0d5d4a320a5e720e1d7762b0708fe1ca63
Gustavo Ramírez Mireles <gusramireles@hotmail.com>
2024-02-20 12:11:03 -0600

    Programación de SELECT, DELETE, INSERT en doctores.class.php

--- admin/doctores.class.php
+++ admin/doctores.class.php
@@ -12,16 +12,36 @@ class Doctores extends Sistema
         return $datos;
     }
 
-    function getOne() {
-        
+    function getOne($id_doctor) {
+        $this->connect();
+        $stmt = $this->conn->prepare("SELECT id_doctor AS ID, CONCAT(nombre, ' ', primer_apellido, ' ', segundo_apellido) AS Nombre, fotografia AS Foto 
+        FROM doctor
+        WHERE id_doctor = :id_doctor;");
+        $stmt->bindParam(':id_doctor', $id_doctor, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $datos = $stmt->fetchAll();
+        return $datos;
     }
 
-    function insert(){
-
+    function insert($datos){
+        $this->connect();
+        $stmt = $this->conn->prepare("INSERT INTO doctor(nombre, primer_apellido, segundo_apellido, fotografia) VALUES (:nombre, :primer_apellido, :segundo_apellido, :fotografia);");
+        $stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt->bindParam("primer_apellido", $datos["primer_apellido"], PDO::PARAM_STR);
+        $stmt->bindParam("segundo_apellido", $datos["segundo_apellido"], PDO::PARAM_STR);
+        $stmt->bindParam("fotografia", $datos["fotografia"], PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->rowCount();
     }
 
-    function delete() {
-
+    function delete($id_doctor) {
+        $this->connect();
+        $stmt = $this->conn->prepare("DELETE FROM doctor WHERE id_doctor = :id_doctor;");
+        $stmt->bindParam(':id_doctor', $id_doctor, PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->rowCount();
+        return $result;
     }
 
     function update() {
--- admin/test.php
+++ admin/test.php
@@ -1,6 +1,17 @@
 <?php
 include ('doctores.class.php');
 $app = new Doctores;
-$datos = $app->getAll();
+/*$id_doctor = $_GET['id_doctor'];
+$datos = $app->getOne($id_doctor);
 print_r($datos);
+$filas = $app->delete($id_doctor);
+echo $filas;
+$datos = $app->getOne($id_doctor);
+print_r($datos);*/
+$doctor['nombre'] = 'Chuck';
+$doctor['primer_apellido'] = 'Norris';
+$doctor['segundo_apellido'] = 'Pérez';
+$doctor['fotografia'] = 'chuck.jpg';
+$fila = $app->insert($doctor);
+echo $fila;
 ?>
